75b598089d12b7f58989f2e0508e8b67c4419bfe
Dominik Chrastecky <dominik@chrastecky.cz>
2023-08-02 02:29:46 +0200

    Add support for apnews

---
+++ src/SiteHandler/ApNewsSiteHandler.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\SiteHandler;
+
+use Rikudou\MemoizeBundle\Attribute\Memoizable;
+use Rikudou\MemoizeBundle\Attribute\Memoize;
+use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\HttpFoundation\Request;
+
+#[Memoizable]
+#[Memoize]
+final class ApNewsSiteHandler implements SiteHandler
+{
+    public function __construct(
+        private HttpBrowser $browser,
+    ) {
+    }
+
+    public function supports(string $url): bool
+    {
+        $host = parse_url($url, PHP_URL_HOST);
+        if (!$host) {
+            return false;
+        }
+
+        return $host === 'www.apnews.com' || $host === 'apnews.com';
+    }
+
+    public function getContent(string $url): string
+    {
+        $crawler = $this->browser->request(Request::METHOD_GET, $url);
+        $parts = $crawler->filter('.RichTextBody > p');
+        $content = '';
+        $count = count($parts);
+        $i = 0;
+        foreach ($parts as $part) {
+            ++$i;
+            if ($i === $count) {
+                break;
+            }
+            $content .= $part->nodeValue . "\n\n";
+        }
+
+        return trim($content);
+    }
+}
