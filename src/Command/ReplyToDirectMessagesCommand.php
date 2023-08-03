c6992d42789fdd4932e3923aed4acd8637510a2e
Dominik Chrastecky <dominik@chrastecky.cz>
2023-08-03 20:57:44 +0200

    Reply to private messages

---
+++ src/Command/ReplyToDirectMessagesCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Command;
+
+use Rikudou\LemmyApi\LemmyApi;
+use Rikudou\LemmyApi\Response\View\PrivateMessageView;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand('app:reply-to-pms')]
+final class ReplyToDirectMessagesCommand extends Command
+{
+    public function __construct(
+        private readonly LemmyApi $api,
+        private readonly string $currentUsername,
+        private readonly string $maintainer,
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $maintainer = $this->api->user()->get($this->maintainer);
+        $maintainerName = $maintainer->name;
+        $maintainerInstance = parse_url($maintainer->actorId, PHP_URL_HOST);
+
+        foreach ($this->getUnreadMessages() as $privateMessage) {
+            $content = $privateMessage->privateMessage->content;
+            $author = $privateMessage->creator->name;
+            $authorInstance = parse_url($privateMessage->creator->actorId, PHP_URL_HOST);
+
+            $textForMaintainer = "Message to bot {$this->currentUsername} from [@{$author}@{$authorInstance}](/u/{$author}@{$authorInstance}):\n\n---\n\n{$content}";
+            $textForSender = "Hi there! I'm a bot and this inbox is not regularly checked. I have forwarded your message to my author, [@{$maintainerName}@{$maintainerInstance}](/u/{$maintainerName}@{$maintainerInstance}).";
+
+            $this->api->currentUser()->sendPrivateMessage(recipient: $maintainer, content: $textForMaintainer);
+            $this->api->currentUser()->sendPrivateMessage(recipient: $privateMessage->creator, content: $textForSender);
+
+            $this->api->currentUser()->markPrivateMessageAsRead($privateMessage->privateMessage);
+        }
+
+        return self::SUCCESS;
+    }
+
+    /**
+     * @return iterable<PrivateMessageView>
+     */
+    private function getUnreadMessages(): iterable
+    {
+        $page = 1;
+        do {
+            $messages = $this->api->currentUser()->getPrivateMessages(page: $page, unreadOnly: true);
+
+            yield from $messages;
+            ++$page;
+        } while (count($messages));
+    }
+}
