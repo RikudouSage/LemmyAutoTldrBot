9a44eed7453868dcdfd2d7016619229ead4b0c26
Dominik Chrastecky <dominik@chrastecky.cz>
2023-10-28 09:39:05 +0200

    Disclose the amount of skipped messages

--- python/source/summarizer.py
+++ python/source/summarizer.py
@@ -2,17 +2,45 @@
 from __future__ import absolute_import
 from __future__ import division, print_function, unicode_literals
 
+import re
 import sys
 
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.models.dom import Sentence
 from sumy.nlp.stemmers import Stemmer
-from sumy.utils import get_stop_words, parse_stop_words
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.utils import parse_stop_words
 
 from data.english import englishStopWords
 
 LANGUAGE = "english"
+
+elided_msg = "**(tldr: {} sentences skipped)**  "
+
+
+class DiscloseElided_LsaSummarizer_Mixin(object):
+    @staticmethod
+    def _get_best_sentences(sentences, count, rate):
+        top_n = sorted(range(len(sentences)), key=rate, reverse=True)[:count]
+
+        elided = 0
+
+        for i, s in enumerate(sentences):
+            if i in top_n:
+                if elided:
+                    yield elided_msg.format(elided)
+                    elided = 0
+                yield s
+            else:
+                elided += 1
+        if elided:
+            yield elided_msg.format(elided)
+
+
+class Summarizer(DiscloseElided_LsaSummarizer_Mixin, Summarizer):
+    pass
+
 
 if __name__ == '__main__':
     text = sys.argv[1]
@@ -23,5 +51,11 @@ if __name__ == '__main__':
     summarizer = Summarizer(stemmer)
     summarizer.stop_words = parse_stop_words(englishStopWords)
 
+    checkPattern = re.compile(r'\*\*\(tldr: \d+ sentences skipped\)\*\* {2}')
+
+    sentence: Sentence
     for sentence in summarizer(parser.document, maxSentences):
-        print(sentence, "\n")
+        if checkPattern.match(str(sentence)):
+            print(sentence)
+        else:
+            print(sentence, "\n")
