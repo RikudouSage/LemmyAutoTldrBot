74aac06a7f466ac642d92bdc8660fbc21ec8172f
Dominik Chrastecky <dominik@chrastecky.cz>
2024-05-21 16:53:49 +0200

    Fix site selector

--- src/SiteHandler/ReutersSiteHandler.php
+++ src/SiteHandler/ReutersSiteHandler.php
@@ -16,11 +16,16 @@ final readonly class ReutersSiteHandler extends AbstractSiteHandler
 
     protected function getSelector(): string
     {
-        return '[class^="article-body__content"] > p';
+        return '[class^="article-body__content"] > div[class^="text__text"]';
     }
 
     protected function ignoreLast(): int
     {
         return 1;
+    }
+
+    protected function getUserAgent(): string
+    {
+        return 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0';
     }
 }
