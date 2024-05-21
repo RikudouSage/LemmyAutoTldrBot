fc15504f3b4a02d2cba96328fe0d832adcb06627
Dominik Chrastecky <dominik@chrastecky.cz>
2024-05-21 17:14:05 +0200

    Fix new site structure

--- src/SiteHandler/WashingtonPostSiteHandler.php
+++ src/SiteHandler/WashingtonPostSiteHandler.php
@@ -16,6 +16,18 @@ final readonly class WashingtonPostSiteHandler extends AbstractSiteHandler
 
     protected function getSelector(): string
     {
-        return '.article-body > p';
+        return '#__NEXT_DATA__';
+    }
+
+    public function getContent(string $url): string
+    {
+        $crawler = $this->getArticleCrawler($url);
+        $scriptContent = $crawler->filter($this->getSelector())->text();
+        $json = json_decode($scriptContent, true);
+        assert(is_array($json));
+        $parts = array_filter($json['props']['pageProps']['globalContent']['content_elements'], static fn (array $element) => $element['type'] === 'text');
+        $parts = array_map(static fn (array $element) => html_entity_decode(strip_tags($element['content'])), $parts);
+
+        return implode("\n\n", $parts);
     }
 }
